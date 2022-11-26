0abf2e997429960d695c26a5eb0b392dff4e63e2
Sergey <Sergeev_sergey99@mail.ru>
2022-11-26 03:35:19 +0300

    init and menu

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
---
+++ Assets/Scripts/OnEventFunction.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnEventFunction : MonoBehaviour
+{
+    public UnityEvent OnAwakeFunctions;
+    public UnityEvent OnStartFunctions;
+    public UnityEvent OnEnableFunctions;
+    public UnityEvent OnDisableFunctions;
+    public UnityEvent OnDestroyFunctions;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        OnAwakeFunctions.Invoke();
+    }
+
+    void Start()
+    {
+        OnStartFunctions.Invoke();
+    }
+
+    private void OnEnable()
+    {
+        OnEnableFunctions.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        OnDisableFunctions.Invoke();
+    }
+
+    private void OnDestroy()
+    {
+        OnDestroyFunctions.Invoke();
+    }
+}
