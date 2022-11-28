d551fb8e0e731a00810ca566af195c35e5efdb61
Sergey <Sergeev_sergey99@mail.ru>
2022-11-28 03:36:11 +0300

    points

--- Assets/PointsController.cs
+++ Assets/PointsController.cs
@@ -1,17 +1,22 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class PointsController : MonoBehaviour
 {
     public PointType pointType;
     public List<Sprite> states;
 
     private MinotaurController player;
+    private Image img;
     private void Start()
     {
-        player = FindObjectOfType<MinotaurController>();
+        if (player == null)
+        {
+            player = FindObjectOfType<MinotaurController>();
+            img = GetComponent<Image>();
+        }
     }
 
     public enum PointType
@@ -21,13 +26,16 @@ public class PointsController : MonoBehaviour
     }
     public void SetPoints(int i)
     {
+        if(player == null) Start();
         if (pointType == PointType.OD)
         {
-            var val = i * 1f / player.MinotaurOD * states.Count;
+            var val = i * 1f / player.MinotaurOD * (states.Count - 1);
+            img.sprite = states[(int) Mathf.Round(val)];
         }
         else
         {
-            
+            var val = i * 1f / player.HungerOG * (states.Count - 1);
+            img.sprite = states[(int) Mathf.Round(val)];
         }
     }
 }
