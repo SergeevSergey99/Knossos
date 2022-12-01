e9c278c63ae4c7eed5bd39c001e15a24f69d41d6
Stefan Pishchikov <stepan.pishchikov@gmail.com>
2022-12-01 22:27:12 +0300

    Speedup+AudioManagerEdit

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -7,9 +7,15 @@ using Random = UnityEngine.Random;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    public AudioClip Clip1;
-    public AudioClip Clip2;
-    public AudioClip Clip3;
+    [Serializable]
+    public class AudioInfo
+    {
+        public AudioClip clip;
+        public bool IsLoop;
+    }
+    public AudioInfo Clip1;
+    public AudioInfo Clip2;
+    public AudioInfo Clip3;
 
     AudioSource AS = null;
     bool secondClip = false;
@@ -24,13 +30,15 @@ public class AudioManager : MonoBehaviour
         RandomPitch(0.1f);
         if (secondClip)
         {
-            AS.clip = Clip2;
+            AS.clip = Clip2.clip;
+            AS.loop = Clip2.IsLoop;
             AS.Play();
             secondClip = false;
         }
         else
         {
-            AS.clip = Clip1;
+            AS.clip = Clip1.clip;
+            AS.loop = Clip1.IsLoop;
             AS.Play();
             secondClip = true;
         }
@@ -41,15 +49,15 @@ public class AudioManager : MonoBehaviour
         switch (Random.Range(0, 3))
         {
             case 0:
-                if (Clip1 == null) PlayRandom();
+                if (Clip1.clip == null) PlayRandom();
                 else PlayFirst();
                 break;
             case 1:
-                if (Clip2 == null) PlayRandom();
+                if (Clip2.clip == null) PlayRandom();
                 else PlaySecond();
                 break;
             case 2:
-                if (Clip3 == null) PlayRandom();
+                if (Clip3.clip == null) PlayRandom();
                 else PlayThird();
                 break;
         }
@@ -57,19 +65,22 @@ public class AudioManager : MonoBehaviour
 
     public void PlayFirst()
     {
-        AS.clip = Clip1;
+        AS.clip = Clip1.clip;
+        AS.loop = Clip1.IsLoop;
         AS.Play();
     }
 
     public void PlaySecond()
     {
-        AS.clip = Clip2;
+        AS.clip = Clip2.clip;
+        AS.loop = Clip2.IsLoop;
         AS.Play();
     }
 
     public void PlayThird()
     {
-        AS.clip = Clip3;
+        AS.clip = Clip3.clip;
+        AS.loop = Clip3.IsLoop;
         AS.Play();
     }
 
